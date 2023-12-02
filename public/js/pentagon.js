9c3aad959f8ca0541ef278760980962d16612f44
ms9849 <65911657+ms9849@users.noreply.github.com>
2023-12-02 19:52:48 +0900

    Add files via upload

---
+++ public/js/pentagon.js
@@ -0,0 +1,463 @@
+"use strict";
+
+var canvas;
+var gl;
+var program;
+
+var projectionMatrix;
+var modelViewMatrix;
+
+var instanceMatrix;
+
+var modelViewMatrixLoc;
+
+var vertices = [
+  vec4(-0.5, -0.5, 0.5, 1.0),
+  vec4(-0.5, 0.5, 0.5, 1.0),
+  vec4(0.5, 0.5, 0.5, 1.0),
+  vec4(0.5, -0.5, 0.5, 1.0),
+  vec4(-0.5, -0.5, -0.5, 1.0),
+  vec4(-0.5, 0.5, -0.5, 1.0),
+  vec4(0.5, 0.5, -0.5, 1.0),
+  vec4(0.5, -0.5, -0.5, 1.0),
+];
+var walkMatrix = mat4();
+var torsoId = 0;
+var headId = 1;
+var head1Id = 1;
+var head2Id = 10;
+var leftUpperArmId = 2;
+var leftLowerArmId = 3;
+var rightUpperArmId = 4;
+var rightLowerArmId = 5;
+var leftUpperLegId = 6;
+var leftLowerLegId = 7;
+var rightUpperLegId = 8;
+var rightLowerLegId = 9;
+
+var torsoHeight = 5.0;
+var torsoWidth = 1.0;
+var upperArmHeight = 3.0;
+var lowerArmHeight = 2.0;
+var upperArmWidth = 0.5;
+var lowerArmWidth = 0.5;
+var upperLegWidth = 0.5;
+var lowerLegWidth = 0.5;
+var lowerLegHeight = 2.0;
+var upperLegHeight = 3.0;
+var headHeight = 1.5;
+var headWidth = 1.0;
+
+var numNodes = 10;
+var numAngles = 11;
+var angle = 70;
+var armAngle = 20;
+var cnt = 0;
+var animDirect = 1;
+
+var theta = [0, 0, 0, 0, 0, 0, 180, 0, 180, 0, 0];
+
+var numVertices = 24;
+
+var stack = [];
+
+var figure = [];
+for (var i = 0; i < numNodes; i++)
+  figure[i] = createNode(null, null, null, null);
+
+var vBuffer;
+var modelViewLoc;
+
+var pointsArray = [];
+var texCoordsArray = [];
+//-------------------------------------------
+var texture;
+
+var texCoord = [vec2(0, 0), vec2(0, 1), vec2(1, 1), vec2(1, 0)];
+function scale4(a, b, c) {
+  var result = mat4();
+  result[0][0] = a;
+  result[1][1] = b;
+  result[2][2] = c;
+  return result;
+}
+function configureTexture(image) {
+  texture = gl.createTexture();
+  gl.bindTexture(gl.TEXTURE_2D, texture);
+  gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
+  gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGB, gl.RGB, gl.UNSIGNED_BYTE, image);
+  gl.generateMipmap(gl.TEXTURE_2D);
+  gl.texParameteri(
+    gl.TEXTURE_2D,
+    gl.TEXTURE_MIN_FILTER,
+    gl.NEAREST_MIPMAP_LINEAR
+  );
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+
+  gl.uniform1i(gl.getUniformLocation(program, "texture"), 0);
+}
+//--------------------------------------------
+
+function createNode(transform, render, sibling, child) {
+  var node = {
+    transform: transform,
+    render: render,
+    sibling: sibling,
+    child: child,
+  };
+  return node;
+}
+
+function initNodes(Id) {
+  var m = mat4();
+
+  switch (Id) {
+    case torsoId:
+      m = rotate(70, 0, 1, 0);
+      figure[torsoId] = createNode(m, torso, null, headId);
+      break;
+
+    case headId:
+    case head1Id:
+    case head2Id:
+      m = translate(0.0, torsoHeight + 0.5 * headHeight, 0.0);
+      m = mult(m, rotate(theta[head1Id], 1, 0, 0));
+      m = mult(m, rotate(theta[head2Id], 0, 1, 0));
+      m = mult(m, translate(0.0, -0.5 * headHeight, 0.0));
+      figure[headId] = createNode(m, head, leftUpperArmId, null);
+      break;
+
+    case leftUpperArmId:
+      m = translate(-(torsoWidth + upperArmWidth), 0.9 * torsoHeight, 0.0);
+      m = mult(m, rotate(theta[leftUpperArmId], 1, 0, 0));
+      figure[leftUpperArmId] = createNode(
+        m,
+        leftUpperArm,
+        rightUpperArmId,
+        leftLowerArmId
+      );
+      break;
+
+    case rightUpperArmId:
+      m = translate(torsoWidth + upperArmWidth, 0.9 * torsoHeight, 0.0);
+      m = mult(m, rotate(theta[rightUpperArmId], 1, 0, 0));
+      figure[rightUpperArmId] = createNode(
+        m,
+        rightUpperArm,
+        leftUpperLegId,
+        rightLowerArmId
+      );
+      break;
+
+    case leftUpperLegId:
+      m = translate(-(torsoWidth + upperLegWidth), 0.1 * upperLegHeight, 0.0);
+      m = mult(m, rotate(theta[leftUpperLegId], 1, 0, 0));
+      figure[leftUpperLegId] = createNode(
+        m,
+        leftUpperLeg,
+        rightUpperLegId,
+        leftLowerLegId
+      );
+      break;
+
+    case rightUpperLegId:
+      m = translate(torsoWidth + upperLegWidth, 0.1 * upperLegHeight, 0.0);
+      m = mult(m, rotate(theta[rightUpperLegId], 1, 0, 0));
+      figure[rightUpperLegId] = createNode(
+        m,
+        rightUpperLeg,
+        null,
+        rightLowerLegId
+      );
+      break;
+
+    case leftLowerArmId:
+      m = translate(0.0, upperArmHeight, 0.0);
+      m = mult(m, rotate(theta[leftLowerArmId], 1, 0, 0));
+      figure[leftLowerArmId] = createNode(m, leftLowerArm, null, null);
+      break;
+
+    case rightLowerArmId:
+      m = translate(0.0, upperArmHeight, 0.0);
+      m = mult(m, rotate(theta[rightLowerArmId], 1, 0, 0));
+      figure[rightLowerArmId] = createNode(m, rightLowerArm, null, null);
+      break;
+
+    case leftLowerLegId:
+      m = translate(0.0, upperLegHeight, 0.0);
+      m = mult(m, rotate(theta[leftLowerLegId], 1, 0, 0));
+      figure[leftLowerLegId] = createNode(m, leftLowerLeg, null, null);
+      break;
+
+    case rightLowerLegId:
+      m = translate(0.0, upperLegHeight, 0.0);
+      m = mult(m, rotate(theta[rightLowerLegId], 1, 0, 0));
+      figure[rightLowerLegId] = createNode(m, rightLowerLeg, null, null);
+      break;
+  }
+}
+
+function traverse(Id) {
+  if (Id == null) return;
+  stack.push(modelViewMatrix);
+  modelViewMatrix = mult(modelViewMatrix, figure[Id].transform);
+  figure[Id].render();
+  if (figure[Id].child != null) traverse(figure[Id].child);
+  modelViewMatrix = stack.pop();
+  if (figure[Id].sibling != null) traverse(figure[Id].sibling);
+}
+
+function torso() {
+  // rendering 함수
+  instanceMatrix = mult(
+    modelViewMatrix,
+    translate(0.0, 0.5 * torsoHeight, 0.0)
+  );
+  instanceMatrix = mult(
+    instanceMatrix,
+    scale4(torsoWidth, torsoHeight, torsoWidth)
+  );
+  gl.uniformMatrix4fv(modelViewMatrixLoc, false, flatten(instanceMatrix));
+  for (var i = 0; i < 6; i++) gl.drawArrays(gl.TRIANGLE_FAN, 4 * i, 4);
+}
+
+function head() {
+  instanceMatrix = mult(modelViewMatrix, translate(0.0, 0.5 * headHeight, 0.0));
+  instanceMatrix = mult(
+    instanceMatrix,
+    scale4(headWidth, headHeight, headWidth)
+  );
+  gl.uniformMatrix4fv(modelViewMatrixLoc, false, flatten(instanceMatrix));
+  for (var i = 0; i < 6; i++) gl.drawArrays(gl.TRIANGLE_FAN, 4 * i, 4);
+}
+
+function leftUpperArm() {
+  instanceMatrix = mult(
+    modelViewMatrix,
+    translate(0.0, 0.5 * upperArmHeight, 0.0)
+  );
+  instanceMatrix = mult(
+    instanceMatrix,
+    scale4(upperArmWidth, upperArmHeight, upperArmWidth)
+  );
+  var leftArmAngle = Math.sin((armAngle * Math.PI) / 180);
+  figure[leftUpperArmId].transform = mult(
+    rotate(leftArmAngle * animDirect, 1, 0, 0),
+    figure[leftUpperArmId].transform
+  );
+  gl.uniformMatrix4fv(modelViewMatrixLoc, false, flatten(instanceMatrix));
+  for (var i = 0; i < 6; i++) gl.drawArrays(gl.TRIANGLE_FAN, 4 * i, 4);
+}
+
+function leftLowerArm() {
+  instanceMatrix = mult(
+    modelViewMatrix,
+    translate(0.0, 0.5 * lowerArmHeight, 0.0)
+  );
+  instanceMatrix = mult(
+    instanceMatrix,
+    scale4(lowerArmWidth, lowerArmHeight, lowerArmWidth)
+  );
+  gl.uniformMatrix4fv(modelViewMatrixLoc, false, flatten(instanceMatrix));
+  for (var i = 0; i < 6; i++) gl.drawArrays(gl.TRIANGLE_FAN, 4 * i, 4);
+}
+
+function rightUpperArm() {
+  instanceMatrix = mult(
+    modelViewMatrix,
+    translate(0.0, 0.5 * upperArmHeight, 0.0)
+  );
+  instanceMatrix = mult(
+    instanceMatrix,
+    scale4(upperArmWidth, upperArmHeight, upperArmWidth)
+  );
+  // 오른쪽 팔 변환 업데이트
+  var rightArmAngle = Math.sin(((armAngle + 180) * Math.PI) / 180);
+  figure[rightUpperArmId].transform = mult(
+    rotate(rightArmAngle * animDirect, 1, 0, 0),
+    figure[rightUpperArmId].transform
+  );
+  gl.uniformMatrix4fv(modelViewMatrixLoc, false, flatten(instanceMatrix));
+  for (var i = 0; i < 6; i++) gl.drawArrays(gl.TRIANGLE_FAN, 4 * i, 4);
+}
+
+function rightLowerArm() {
+  instanceMatrix = mult(
+    modelViewMatrix,
+    translate(0.0, 0.5 * lowerArmHeight, 0.0)
+  );
+  instanceMatrix = mult(
+    instanceMatrix,
+    scale4(lowerArmWidth, lowerArmHeight, lowerArmWidth)
+  );
+  gl.uniformMatrix4fv(modelViewMatrixLoc, false, flatten(instanceMatrix));
+  for (var i = 0; i < 6; i++) gl.drawArrays(gl.TRIANGLE_FAN, 4 * i, 4);
+}
+
+function leftUpperLeg() {
+  instanceMatrix = mult(
+    modelViewMatrix,
+    translate(0.0, 0.5 * upperLegHeight, 0.0)
+  );
+  instanceMatrix = mult(
+    instanceMatrix,
+    scale4(upperLegWidth, upperLegHeight, upperLegWidth)
+  );
+  // 왼쪽 다리 애니메이션
+  var leftLegAngle = Math.sin((angle * Math.PI) / 180);
+  figure[leftUpperLegId].transform = mult(
+    rotate(leftLegAngle * animDirect, 1, 0, 0),
+    figure[leftUpperLegId].transform
+  );
+  gl.uniformMatrix4fv(modelViewMatrixLoc, false, flatten(instanceMatrix));
+  for (var i = 0; i < 6; i++) gl.drawArrays(gl.TRIANGLE_FAN, 4 * i, 4);
+}
+
+function leftLowerLeg() {
+  instanceMatrix = mult(
+    modelViewMatrix,
+    translate(0.0, 0.5 * lowerLegHeight, 0.0)
+  );
+  instanceMatrix = mult(
+    instanceMatrix,
+    scale4(lowerLegWidth, lowerLegHeight, lowerLegWidth)
+  );
+  gl.uniformMatrix4fv(modelViewMatrixLoc, false, flatten(instanceMatrix));
+  for (var i = 0; i < 6; i++) gl.drawArrays(gl.TRIANGLE_FAN, 4 * i, 4);
+}
+
+function rightUpperLeg() {
+  instanceMatrix = mult(
+    modelViewMatrix,
+    translate(0.0, 0.5 * upperLegHeight, 0.0)
+  );
+  instanceMatrix = mult(
+    instanceMatrix,
+    scale4(upperLegWidth, upperLegHeight, upperLegWidth)
+  );
+  // 오른쪽 다리 애니메이션
+  var rightLegAngle = Math.sin(((angle + 180) * Math.PI) / 180);
+  figure[rightUpperLegId].transform = mult(
+    rotate(rightLegAngle * animDirect, 1, 0, 0),
+    figure[rightUpperLegId].transform
+  );
+  gl.uniformMatrix4fv(modelViewMatrixLoc, false, flatten(instanceMatrix));
+  for (var i = 0; i < 6; i++) gl.drawArrays(gl.TRIANGLE_FAN, 4 * i, 4);
+}
+
+function rightLowerLeg() {
+  instanceMatrix = mult(
+    modelViewMatrix,
+    translate(0.0, 0.5 * lowerLegHeight, 0.0)
+  );
+  instanceMatrix = mult(
+    instanceMatrix,
+    scale4(lowerLegWidth, lowerLegHeight, lowerLegWidth)
+  );
+  gl.uniformMatrix4fv(modelViewMatrixLoc, false, flatten(instanceMatrix));
+  for (var i = 0; i < 6; i++) gl.drawArrays(gl.TRIANGLE_FAN, 4 * i, 4);
+}
+
+function quad(a, b, c, d) {
+  pointsArray.push(vertices[a]);
+  texCoordsArray.push(texCoord[0]);
+
+  pointsArray.push(vertices[b]);
+  texCoordsArray.push(texCoord[1]);
+
+  pointsArray.push(vertices[c]);
+  texCoordsArray.push(texCoord[2]);
+
+  pointsArray.push(vertices[d]);
+  texCoordsArray.push(texCoord[3]);
+}
+
+function cube() {
+  quad(1, 0, 3, 2);
+  quad(2, 3, 7, 6);
+  quad(3, 0, 4, 7);
+  quad(6, 5, 1, 2);
+  quad(4, 5, 6, 7);
+  quad(5, 4, 0, 1);
+}
+// window on load
+//
+//
+//
+//
+window.onload = function init() {
+  canvas = document.getElementById("gl-canvas");
+
+  gl = WebGLUtils.setupWebGL(canvas);
+  if (!gl) {
+    alert("WebGL isn't available");
+  }
+
+  gl.viewport(0, 0, canvas.width, canvas.height);
+  gl.clearColor(1.0, 1.0, 1.0, 1.0);
+  //
+  //  Load shaders and initialize attribute buffers
+  //
+  program = initShaders(gl, "vertex-shader", "fragment-shader");
+
+  gl.useProgram(program);
+
+  instanceMatrix = mat4();
+
+  projectionMatrix = ortho(-10.0, 10.0, -10.0, 10.0, -10.0, 10.0);
+  modelViewMatrix = mat4();
+
+  gl.uniformMatrix4fv(
+    gl.getUniformLocation(program, "modelViewMatrix"),
+    false,
+    flatten(modelViewMatrix)
+  );
+  gl.uniformMatrix4fv(
+    gl.getUniformLocation(program, "projectionMatrix"),
+    false,
+    flatten(projectionMatrix)
+  );
+
+  modelViewMatrixLoc = gl.getUniformLocation(program, "modelViewMatrix");
+
+  var image = document.getElementById("texImage");
+  configureTexture(image);
+
+  cube();
+
+  vBuffer = gl.createBuffer();
+
+  gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
+  gl.bufferData(gl.ARRAY_BUFFER, flatten(pointsArray), gl.STATIC_DRAW);
+
+  var vPosition = gl.getAttribLocation(program, "vPosition");
+  gl.vertexAttribPointer(vPosition, 4, gl.FLOAT, false, 0, 0);
+  gl.enableVertexAttribArray(vPosition);
+
+  var tBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, tBuffer);
+  gl.bufferData(gl.ARRAY_BUFFER, flatten(texCoordsArray), gl.STATIC_DRAW);
+
+  var vTexCoord = gl.getAttribLocation(program, "vTexCoord");
+  gl.vertexAttribPointer(vTexCoord, 2, gl.FLOAT, false, 0, 0);
+  gl.enableVertexAttribArray(vTexCoord);
+  gl.uniformMatrix4fv(modelViewMatrixLoc, false, flatten(instanceMatrix));
+
+  for (i = 0; i < numNodes; i++) initNodes(i);
+  render();
+};
+
+var render = function () {
+  gl.clear(gl.COLOR_BUFFER_BIT);
+  traverse(torsoId);
+  // 각 노드에 대해 걷는 동안 적용할 변환 행렬을 지정합니다.
+  // 각 프레임마다 각 노드의 변환 행렬을 업데이트합니다.
+  // 방향 지정 애니메이션
+  cnt++;
+  if (cnt == 20) {
+    cnt = -20;
+    animDirect *= -1;
+  }
+  // 다음 프레임의 각도를 증가시킵니다.
+  requestAnimFrame(render);
+};
